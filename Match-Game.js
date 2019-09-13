228620eedb52c46ad18967fed988463498fb4f01
Logan Trecartin <ltrecartin@admins-MacBook-Pro-2.local>
2019-09-13 16:42:11 -0400

    step 2 showing electron

---
+++ Match-Game.js
@@ -0,0 +1,19 @@
+var config = {
+    type: Phaser.WEBGL,
+    width: 800,
+    height: 600,
+    parent: 'match-game',
+    // physics: {
+    //     default: 'arcade',
+    //     arcade: {
+    //         gravity: { y: 200 }
+    //     }
+    // },
+    scene: this
+};
+
+var game = new Phaser.Game(config);
+
+function create(){
+
+}
